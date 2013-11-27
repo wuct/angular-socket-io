e35a4925a6845a27beceafc95d9c4fde20a45471
wuct <wu.chingting@gmail.com>
2013-11-28 02:51:48 +0800

    fix uglify issue

--- socket.js
+++ socket.js
@@ -1,3 +1,6 @@
+// fix uglify problem
+// wuct @ 2013.11.28
+
 /*
  * angular-socket-io v0.2.0
  * (c) 2013 Brian Ford http://briantford.com
@@ -7,77 +10,77 @@
 'use strict';
 
 angular.module('btford.socket-io', []).
-  provider('socket', function () {
+	provider('socket', function () {
 
-    // when forwarding events, prefix the event name
-    var prefix = 'socket:',
-      ioSocket;
+		// when forwarding events, prefix the event name
+		var prefix = 'socket:',
+			ioSocket;
 
-    // expose to provider
-    this.$get = function ($rootScope, $timeout) {
+		// expose to provider
+		this.$get = ['$rootScope', '$timeout', function ($rootScope, $timeout) {
 
-      var socket = ioSocket || io.connect();
+			var socket = ioSocket || io.connect();
 
-      var asyncAngularify = function (callback) {
-        return function () {  
-          var args = arguments;
-          $timeout(function () {
-            callback.apply(socket, args);
-          }, 0);
-        };
-      };
+			var asyncAngularify = function (callback) {
+				return function () {	
+					var args = arguments;
+					$timeout(function () {
+						callback.apply(socket, args);
+					}, 0);
+				};
+			};
 
-      var addListener = function (eventName, callback) {
-        socket.on(eventName, asyncAngularify(callback));
-      };
+			var addListener = function (eventName, callback) {
+				socket.on(eventName, asyncAngularify(callback));
+			};
 
-      var wrappedSocket = {
-        on: addListener,
-        addListener: addListener,
+			var wrappedSocket = {
+				on: addListener,
+				addListener: addListener,
 
-        emit: function (eventName, data, callback) {
-          if (callback) {
-            socket.emit(eventName, data, asyncAngularify(callback));
-          } else {
-            socket.emit(eventName, data);
-          }
-        },
+				emit: function (eventName, data, callback) {
+					if (callback) {
+						socket.emit(eventName, data, asyncAngularify(callback));
+					} else {
+						socket.emit(eventName, data);
+					}
+				},
 
-        removeListener: function () {
-          var args = arguments;
-          return socket.removeListener.apply(socket, args);
-        },
+				removeListener: function () {
+					var args = arguments;
+					return socket.removeListener.apply(socket, args);
+				},
 
-        // when socket.on('someEvent', fn (data) { ... }),
-        // call scope.$broadcast('someEvent', data)
-        forward: function (events, scope) {
-          if (events instanceof Array === false) {
-            events = [events];
-          }
-          if (!scope) {
-            scope = $rootScope;
-          }
-          events.forEach(function (eventName) {
-            var prefixed = prefix + eventName;
-            var forwardEvent = asyncAngularify(function (data) {
-              scope.$broadcast(prefixed, data);
-            });
-            scope.$on('$destroy', function () {
-              socket.removeListener(eventName, forwardEvent);
-            });
-            socket.on(eventName, forwardEvent);
-          });
-        }
-      };
+				// when socket.on('someEvent', fn (data) { ... }),
+				// call scope.$broadcast('someEvent', data)
+				forward: function (events, scope) {
+					if (events instanceof Array === false) {
+						events = [events];
+					}
+					if (!scope) {
+						scope = $rootScope;
+					}
+					events.forEach(function (eventName) {
+						var prefixed = prefix + eventName;
+						var forwardEvent = asyncAngularify(function (data) {
+							scope.$broadcast(prefixed, data);
+						});
+						scope.$on('$destroy', function () {
+							socket.removeListener(eventName, forwardEvent);
+						});
+						socket.on(eventName, forwardEvent);
+					});
+				}
+			};
 
-      return wrappedSocket;
-    };
+			return wrappedSocket;
+		}];
 
-    this.prefix = function (newPrefix) {
-      prefix = newPrefix;
-    };
+		this.prefix = function (newPrefix) {
+			prefix = newPrefix;
+		};
 
-    this.ioSocket = function (socket) {
-      ioSocket = socket;
-    };
-  });
+		this.ioSocket = function (socket) {
+			ioSocket = socket;
+		};
+	});
